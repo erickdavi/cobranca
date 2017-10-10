bda6488b7278e1f68ca1aeb93dc6ac063f576219
erickdavi <erickdavi@gmail.com>
2017-10-10 17:55:45 -0300

    Avanços 10-10-2016

--- class/main.rb
+++ class/main.rb
@@ -1,3 +1,4 @@
+
 class Cobranca
 	attr_accessor :ano
 	def initialize(ano)
@@ -9,10 +10,13 @@ class Cobranca
 		@dir_input = ["../in/cadastro/", "../in/devedores/"]
 		@arq_base0 = @dir_input[0]+ano.to_s+'.csv'
 		@arq_base1 = @dir_input[1]+ano.to_s+'.csv'
-		@arq_output = "../out/"+ano.to_s+".csv"		
+		@arq_output = "../out/"+ano.to_s+".csv"	
+		
+		self.init_cad
+		self.init_dev	
 	end
 
-	def init_cad
+	protected def init_cad
 		if File.exist?(@arq_base0)
 			File.open(@arq_base0, 'r') do |arquivo|
 				while linha = arquivo.gets
@@ -21,56 +25,62 @@ class Cobranca
 					{
 						Mat: coluna[0], Nome: coluna[1], Telefone: coluna[2], TelEmpresaMae: coluna[3], 
 						TelEmpresaPai: coluna[4], AnoLetivo: coluna[5], Descricao: coluna[6], 
-						Serie: coluna[7], Turma: coluna[8], Turno: coluna[9], Numero: coluna[10], 
-						FilhoFuncionario: coluna[11],Situacao: coluna[12], NomeMae: coluna[13], 
+						Serie: coluna[7], Turma: coluna[8], Turno: coluna[9], Numero: coluna[10],
+						FilhoFuncionario: coluna[11],Situacao: coluna[12], NomeMae: coluna[13],
 						NomePai: coluna[14], NomeResp: coluna[15]
 					}
 					@cache_cadastro.push(cacheline)
-					out = true
 				end
-			end	
+			end
 			@cache_cadastro.shift
-			out = @cache_cadastro
+			cache_temp = @cache_cadastro
+			@cache_cadastro = cache_temp.sort_by{|hsh| hsh[:Mat]}
+			out = true
 		else
 			out = false
-		end			
-	end		
+		end
+	end
 
-	def init_dev
-		if File.exist?(@arq_base1)	
+	protected def init_dev
+		if File.exist?(@arq_base1)
 			File.open(@arq_base1, 'r') do |arquivo|
 				while linha = arquivo.gets
 					coluna = linha.split(',')
-					cacheline = 
-						{				
+					cacheline =
+						{
 							Mat: coluna[0],	Descricao: coluna[1],	DtVenc: coluna[2],	Valor: coluna[3],	
 							NomeResp: coluna[4], TelefoneResp: coluna[5], cpfresp: coluna[6]
 						}
-				@cache_devedores.push(cacheline)
-				out = true
+					@cache_devedores.push(cacheline)
 				end
 			end
-		@cache_devedores.shift
-		out = @cache_devedores		
+			@cache_devedores.shift
+			cache_temp = @cache_devedores
+			@cache_devedores = cache_temp.sort_by {|hsh| hsh[:Mat]}
+			out = true
 		else
 			out = false
-		end			
-	end	
+		end
+	end
+	
+	def get_cache(esp)
+		if esp == "dev"
+			out = @cache_devedores
+		elsif esp == "cad"
+			out = @cache_cadastro
+		else
+			out = false			
+		end	
+	end
 
 	def indice_cadastro(mat)
 		cadastro_indice = self.init_cad.index do |cad_linha|
 			cad_linha[:Mat] == mat
 		end
-	end					
-	def indice_devedores
+	end
+	def indice_devedores(mat)
 		devedores_indice = self.init_dev.index do |dev_linha|
-			dev_linha[:Mat] = 2899
+			dev_linha[:Mat] == mat
 		end
-	end		
-end		
-		
-
-A = Cobranca.new(2013)
-
-puts A.indice_devedores
-
+	end
+end
